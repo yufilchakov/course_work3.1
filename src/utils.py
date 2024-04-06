b26fd82df54985c62bf7cd63d295a209b570e450
Yura Filchakov <filchakov1993@gmail.com>
2024-04-07 03:33:41 +0700

    sort_date

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import json
-
+from datetime import datetime
 
 def load_operations_json(file_name):
     with open(file_name, 'r', encoding='utf-8') as load_file:
@@ -10,3 +10,9 @@ def load_operations_json(file_name):
 def list_of_operations(operations):
     operations = list(filter(lambda x: len(x) and x['state'] == 'EXECUTED', operations))
     return operations
+
+
+def sort_date(json_operations):
+    json_date_time = sorted(json_operations, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'),
+                            reverse=True)
+    return json_date_time
