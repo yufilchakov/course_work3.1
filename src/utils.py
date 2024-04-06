57875bbbe1ed505cb9f5f5b536ec9d9f45b93371
Yura Filchakov <filchakov1993@gmail.com>
2024-04-07 03:32:46 +0700

    list_of_operations

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,8 @@ def load_operations_json(file_name):
     with open(file_name, 'r', encoding='utf-8') as load_file:
         operations = json.load(load_file)
     return operations
+
+
+def list_of_operations(operations):
+    operations = list(filter(lambda x: len(x) and x['state'] == 'EXECUTED', operations))
+    return operations
